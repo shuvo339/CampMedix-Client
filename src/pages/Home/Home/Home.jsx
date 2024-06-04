c8e7741e27e0b464c5b861e9f18e2cf17bc67148
shuvo339 <shubhroshafiullah@gmail.com>
2024-06-04 19:51:26 +0600

    show feedback on homepage

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import PopularCamps from "../PopularCamps/PopularCamps";
+import ShowFeedback from "../ShowFeedback/ShowFeedback";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <PopularCamps></PopularCamps>
+            <ShowFeedback></ShowFeedback>
         </div>
     );
 };
